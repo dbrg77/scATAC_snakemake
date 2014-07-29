b6e9ac574baac076188e7affeb256b7854a03307
Xi Chen <dbrg77@gmail.com>
2014-07-29 23:43:27 +0100

    Update Fragment_length_density_plot.py

--- Fragment_length_density_plot.py
+++ Fragment_length_density_plot.py
@@ -18,19 +18,19 @@ data = np.empty((len(files), 2, 1200))
 frags = []
 
 for i,j in enumerate(files):
-	frags.append([])
-	bam = pysam.Samfile(j, 'rb')
-	for read in bam:
-		if bam.getrname(read.tid)!="chrM" and read.tlen>0 and read.tlen<1200:
-			frags[i].append(read.tlen)
-	bam.close()
-	hist, b = np.histogram(frags[i], bins, density=True)
-	data[i][0] = (b[1:]+b[:-1])/2
-	data[i][1] = hist
+frags.append([])
+bam = pysam.Samfile(j, 'rb')
+for read in bam:
+if bam.getrname(read.tid)!="chrM" and read.tlen>0 and read.tlen<1200:
+frags[i].append(read.tlen)
+bam.close()
+hist, b = np.histogram(frags[i], bins, density=True)
+data[i][0] = (b[1:]+b[:-1])/2
+data[i][1] = hist
 
 for i,j in enumerate(data):
-	xs, ys = j
-	plt.plot(xs, ys, colour[i]+'-', label=labels[i], alpha=0.7)
+xs, ys = j
+plt.plot(xs, ys, colour[i]+'-', label=labels[i], alpha=0.7)
 plt.xticks(range(0,1200,200))
 plt.xlim(0,1200)
 plt.xlabel('Fragment length (bp)')
@@ -42,10 +42,10 @@ plt.clf()
 ymax = 0
 
 for i,j in enumerate(data):
-	xs, ys = j
-	if max(ys) > ymax:
-		ymax = max(ys)
-	plt.plot(xs, ys, colour[i]+'-', label=labels[i], alpha=0.7)
+xs, ys = j
+if max(ys) > ymax:
+ymax = max(ys)
+plt.plot(xs, ys, colour[i]+'-', label=labels[i], alpha=0.7)
 plt.xticks(range(0,1200,200))
 plt.xlim(0,1200)
 plt.yscale('log')
