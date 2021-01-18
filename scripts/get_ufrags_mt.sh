18051141734bb701bc7cad50e424841e9fc6096f
Xi Chen <dbrg77@gmail.com>
2021-01-18 17:20:36 +0800

    Update get_ufrags_mt.sh

--- scripts/get_ufrags_mt.sh
+++ scripts/get_ufrags_mt.sh
@@ -9,7 +9,7 @@ for i in */picard_bam/*.bam; do
     tread=$(samtools idxstats ${i} | addCols stdin | awk '{print $3}')
     mt=$(samtools idxstats ${i} | grep chrM | awk '{print $3}')
     nread=$(calc ${tread}-${mt} | awk '{print $3}')
-    nfrag=$(calc ${nread}/2 | awk '{print $3}')
+    nfrag=$(calc ${nread}/2 | awk '{print $3}' | cut -f 1 -d '.')
     p=$(calc ${mt}/${tread}*100 | awk '{print $3}')
     echo -e "${batch}_${cell%_f2q30_pmd.bam}\t${nfrag}" >> qc_metrics/uniq_nuc_frags.txt
     echo -e "${batch}_${cell%_f2q30_pmd.bam}\t${p}" >> qc_metrics/mt_content.txt
