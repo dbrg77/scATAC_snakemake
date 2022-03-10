58bf783bc7afc0ce11ea194b4dca73ece8fe7c99
dbrg77 <Xi Chen>
2022-03-10 09:38:51 +0800

    corrected a cell name issue during fragment generation

--- scripts/generate_fragments_file.sh
+++ scripts/generate_fragments_file.sh
@@ -1,7 +1,8 @@
 for i in */picard_bam/*.bam; do
+    batch=$(echo ${i} | cut -f 1 -d/)
     fn=$(echo ${i} | rev | cut -f 1 -d/ | rev)
     cell=${fn%_f2q30_pmd.bam}
     samtools sort -@ 12 -n -T ${cell}_tmp ${i} | \
     bamToBed -bedpe -i - | \
-    awk -v cn=${cell} 'BEGIN{OFS="\t"}{ if($9=="+"){print $1, $2, $6, cn, "1"} else {print $1, $5, $3, cn, "1"} }'
+    awk -v cn="${batch}_${cell}" 'BEGIN{OFS="\t"}{ if($9=="+"){print $1, $2, $6, cn, "1"} else {print $1, $5, $3, cn, "1"} }'
 done
